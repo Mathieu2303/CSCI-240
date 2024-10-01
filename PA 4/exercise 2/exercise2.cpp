ed43c9d0790a52a47a8d313d8c8ef98d13b9b701
Mathieu2303 <mathieupartain@gmail.com>
2024-10-01 16:40:44 -0700

    setup

--- PA 4/exercise 2/exercise2.cpp
+++ PA 4/exercise 2/exercise2.cpp
@@ -6,9 +6,44 @@ using namespace std;
 template <typename T>
 class MyArrayStack{
 private:
-
+    int const SIZE = 10;
+    int array[SIZE];
 public:
 
+
+    void size(){
+        
+        int counter = 0;
+        
+            }
+                
+        }
+    }
+    void empty(){}
+    void top(){}
+    void push(template T){}
+    void pop(){}
+    
+    int evaluatePostfix(const std::string &expression) {
+    MyArrayStack<int> stack;
+    std::stringstream ss(expression);
+    std::string token;
+
+    while (ss >> token) {
+        if (isdigit(token[0]) || (token[0] == '-' && token.size() > 1)) {
+            stack.push(std::stoi(token));
+        } else {
+            int op2 = stack.pop();
+            int op1 = stack.pop();
+            if (token == "+") stack.push(op1 + op2);
+            else if (token == "-") stack.push(op1 - op2);
+            else if (token == "*") stack.push(op1 * op2);
+            else if (token == "/") stack.push(op1 / op2);
+        }
+    }
+
+    return stack.top();
+}
 };
 int main(){
 
